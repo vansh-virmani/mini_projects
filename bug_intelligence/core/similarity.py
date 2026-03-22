90f4bc3fa0b94d8e679b629ef2615ae8179666b1
Vivek-afk81 <vivekchauhan560999@gmail.com>
2026-03-22 19:08:15 +0530

    similarityv1 done

--- bug_intelligence/core/similarity.py
+++ bug_intelligence/core/similarity.py
@@ -10,3 +10,21 @@ def cosine_similarity(vec1,vec2):
     norm2=np.linalg.norm(vec2)
 
     return dot_product /(norm1*norm2)
+
+def find_similar(new_embeddings,database,threshold=0.7):
+    results=[]
+
+    for bug in database:
+        sim=cosine_similarity(new_embeddings,bug["embedding"])
+
+        if sim>threshold:
+            results.append({
+                "text":bug["text"],
+                "similarity":round(sim,2)
+
+            })
+
+    #sorting by similarity
+    results.sort(key=lambda x: x["similarity"],reverse=True)
+
+    return results
--- bug_intelligence/main.py
+++ bug_intelligence/main.py
@@ -1,8 +1,29 @@
 from core.embedder import embed_text
 
+from core.similarity import find_similar
+
+
+bug_database = [
+    {
+        "text": "cannot concatenate str and int",
+        "embedding": embed_text("cannot concatenate str and int")
+    },
+    {
+        "text": "list index out of range",
+        "embedding": embed_text("list index out of range")
+    },
+]
+
+
+
+
 if __name__=="__main__":
     text=input("Enter bug: ")
 
     embeddings=embed_text(text)
+    similar=find_similar(embeddings,bug_database)
 
-    print("embeddings shape",len(embeddings))
+    print("\nSimilar Bugs:") 
+    for item in similar: 
+        print(f"{item['text']} → {item['similarity']:.2f}")
+
